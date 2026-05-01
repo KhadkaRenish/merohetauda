358aa8a26fce116327e476ebec103e89902301a3
Renish Khadka <renishkhadka@Renishs-MacBook-Pro.local>
2026-05-01 22:17:01 +0545

    Connect Expo news screen to Django API

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-@13^#)fmb9_afxc#1su_ef1liosoa503mafy$-ffy4&07&($hf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.1.12"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
